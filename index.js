25d7db7eba9eb74d21e4ac25283c5e37011af4b9
Shakhdv <shakhidov063@mail.ru>
2023-11-24 11:30:09 +0300

    fix conflict

--- index.js
+++ index.js
@@ -11,16 +11,12 @@ app.use(express.json());
 app.use("/images", express.static(path.resolve(__dirname, "images")));
 app.use(require("./routes/user.route"));
 app.use(require("./routes/excursion.route"));
-app.use(require("./routes/tours.route"))
-app.use(require("./routes/categories.route"))
-app.use(require("./routes/regions.route"))
-app.use(require('./routes/comments.route'))
-<<<<<<< HEAD
-app.use(require('./routes/guide.route'))
-=======
-app.use(require("./routes/orders.route"))
->>>>>>> main
-
+app.use(require("./routes/tours.route"));
+app.use(require("./routes/categories.route"));
+app.use(require("./routes/regions.route"));
+app.use(require("./routes/comments.route"));
+app.use(require("./routes/guide.route"));
+app.use(require("./routes/orders.route"));
 mongoose.connect(process.env.MONGO_SERVER);
 
 app.listen(process.env.PORT, () => {
